d46f72f76b2f67bd01f42aeee25969ac46765ad8
Kobayashi-UwU <tai242415597@gmail.com>
2023-06-01 10:56:39 +0700

    update face check

--- addimage.py
+++ addimage.py
@@ -1,9 +1,7 @@
 import cv2
-<<<<<<< HEAD
 import face_recognition
 import os
-=======
->>>>>>> dd92d4867b19a32bac022fb3c2ad506d16ce2065
+
 
 name_of_image = input("Enter your Name: ")
 
@@ -24,7 +22,6 @@ if name_of_image != None:
             break
     cap.release()
     cv2.destroyAllWindows()
-<<<<<<< HEAD
     try:
         face_encodings = face_recognition.face_encodings(
             f"./add_to_database/{name_of_image}.jpg"
@@ -32,9 +29,5 @@ if name_of_image != None:
     except Exception as e:
         print("Error: not found humen face")
         os.remove(f"./add_to_database/{name_of_image}.jpg")
-
-#
-=======
->>>>>>> dd92d4867b19a32bac022fb3c2ad506d16ce2065
 else:
     print("Need name of picture")
